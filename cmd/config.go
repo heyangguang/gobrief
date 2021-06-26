8dcca3cb37bc9e1f38de70aa18ed5dcf46f6f02e
heyang <heyangev@cn.ibm.com>
2021-06-27 01:41:53 +0800

    变更配置文件方式，简化配置

--- cmd/config.go
+++ cmd/config.go
@@ -5,11 +5,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+type TotalConfig struct {
+	LogPath, LogLevel, DBConnect string
+	Port                         int
+}
+
+var AllConfig TotalConfig
+
 func viperInit(config string) {
 	viper.SetConfigFile(config)
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
+	err = viper.Unmarshal(&AllConfig)
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
--- cmd/run.go
+++ cmd/run.go
@@ -4,19 +4,14 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"gobrief/gobrief/dbs"
 	. "gobrief/gobrief/form_validation"
 	"gobrief/gobrief/logger"
 	"gobrief/gobrief/router"
 )
 
-var port int
-var log string
 var logLevel string
-var db string
 var configPath string
-var server bool
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
@@ -25,27 +20,20 @@ var runCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// 初始化配置文件
 		viperInit(configPath)
-		if server {
-			runStart(viper.GetString("log_path"),
-				viper.GetString("log_level"),
-				viper.GetString("db_connect"),
-				viper.GetInt("port"))
-			return
-		} else {
-			runStart(log, logLevel, db, port)
-		}
+		fmt.Println(AllConfig)
+		runStart()
 	},
 }
 
-func runStart(logPath, logLevel, db string, port int) {
+func runStart() {
 	// 日志
-	err := logger.InitLogger(logPath, 2048, 30, 200, logLevel)
+	err := logger.InitLogger(AllConfig.LogPath, 2048, 30, 200, AllConfig.LogLevel)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	// 数据库
-	dbs.InitDB(db)
+	dbs.InitDB(AllConfig.DBConnect)
 	dbs.InitTable()
 
 	// 表单验证
@@ -54,20 +42,15 @@ func runStart(logPath, logLevel, db string, port int) {
 	// !DEBUG
 	if logLevel != "DEBUG" {
 		gin.SetMode(gin.ReleaseMode)
-		logger.Info(fmt.Sprintf("Listening and serving HTTP on :%d", port))
+		logger.Info(fmt.Sprintf("Listening and serving HTTP on :%d", AllConfig.Port))
 	}
 
 	r := router.InitRouter()
 	// TODO:后面会增加容错与优雅退出
-	_ = r.Run(fmt.Sprintf(":%d", port))
+	_ = r.Run(fmt.Sprintf(":%d", AllConfig.Port))
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-	runCmd.Flags().StringVarP(&log, "log", "l", "./logs/go-brief.log", "--log <logLocation> example --log ./logs/go-brief.log")
-	runCmd.Flags().IntVarP(&port, "port", "p", 8080, "--port <port> example --port 8080")
-	runCmd.Flags().StringVar(&logLevel, "logLevel", "INFO", "--logLevel <logLocation> example --logLevel INFO")
-	runCmd.Flags().StringVar(&db, "db", "", "--db <logLocation> example --db root:123456@(127.0.0.1:3306)/test")
-	runCmd.Flags().BoolVar(&server, "server", true, "--server")
 	runCmd.Flags().StringVar(&configPath, "config", "./config_dev.yaml", "--config ./config_prod.yaml")
 }
